660189257fe6d9f88ff58aefad2dd89fff368e7b
Greg <50383853+th3x3rx3s@users.noreply.github.com>
2024-12-19 19:05:22 +0100

    Delete Selection_sort directory

--- Selection_sort/sort.py
+++
@@ -1,8 +0,0 @@
-def selection_sort(lista):
-    for x in range(len(lista)):
-        minindex = x
-        for y in range(x+1, len(lista)):
-            if lista[minindex]>lista[y]:
-                minindex = y
-        lista[x], lista[minindex] = lista[minindex], lista[x]
-    return lista
